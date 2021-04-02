cc29cb13a5de3cc190c796eed501d98afd44ded2
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-04-02 22:09:45 +0300

    Build fix

--- robotframework_reportportal/model.py
+++ robotframework_reportportal/model.py
@@ -111,5 +111,5 @@ class LogMessage:
         self.message = message
 
     def __repr__(self):
-        """Controls string representation of the object"""
+        """The method controls string representation of the object."""
         return self.message
