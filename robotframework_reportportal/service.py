e06d6951112cfd068ed082d630315040e519575f
Ivan Ivanou <ivan_ivanou@epam.com>
2021-04-07 16:34:07 +0300

    Global agent cleanup

--- robotframework_reportportal/service.py
+++ robotframework_reportportal/service.py
@@ -11,6 +11,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+from dateutil.parser import isoparse
 import logging
 
 from reportportal_client.external.google_analytics import send_event
@@ -24,33 +25,31 @@ from reportportal_client.service import (
     ReportPortalService
 )
 
+from .exception import RobotServiceException
+from .static import LOG_LEVEL_MAPPING, STATUS_MAPPING
+
+logger = logging.getLogger(__name__)
+
+
+def to_epoch(date):
+    """Convert Robot Framework timestamp to UTC timestamp."""
+    try:
+        iso_date = isoparse(date)
+    except ValueError:
+        return ''
+    return str(int(iso_date.timestamp() * 1000))
+
 
 class RobotService(object):
     """Class represents service that sends Robot items to Report Portal."""
 
-    agent_name = "robotframework-reportportal"
-    agent_version = get_package_version(agent_name)
-    rp = None
+    def __init__(self):
+        """Initialize service attributes."""
+        self.agent_name = 'robotframework-reportportal'
+        self.agent_version = get_package_version(self.agent_name)
+        self.rp = None
 
-    status_mapping = {
-        "PASS": "PASSED",
-        "FAIL": "FAILED",
-        "NOT RUN": "SKIPPED",
-        "SKIP": "SKIPPED"
-    }
-
-    log_level_mapping = {
-        "INFO": "INFO",
-        "FAIL": "ERROR",
-        "TRACE": "TRACE",
-        "DEBUG": "DEBUG",
-        "HTML": "INFO",
-        "WARN": "WARN",
-        "ERROR": "ERROR"
-    }
-
-    @staticmethod
-    def _get_launch_attributes(cmd_attrs):
+    def _get_launch_attributes(self, cmd_attrs):
         """Generate launch attributes including both system and user ones.
 
         :param list cmd_attrs: List for attributes from the command line
@@ -58,12 +57,10 @@ class RobotService(object):
         attributes = cmd_attrs or []
         system_attributes = get_launch_sys_attrs()
         system_attributes['agent'] = (
-            '{}-{}'.format(RobotService.agent_name,
-                           RobotService.agent_version))
+            '{}-{}'.format(self.agent_name, self.agent_version))
         return attributes + _dict_to_payload(system_attributes)
 
-    @staticmethod
-    def init_service(endpoint, project, uuid, log_batch_size, pool_size):
+    def init_service(self, endpoint, project, uuid, log_batch_size, pool_size):
         """Initialize common reportportal client.
 
         :param endpoint:       Report Portal API endpoint
@@ -72,213 +69,182 @@ class RobotService(object):
         :param log_batch_size: Number of logs to be sent within one batch
         :param pool_size:      HTTPAdapter max pool size
         """
-        if RobotService.rp is None:
-            logging.debug(
-                "ReportPortal - Init service: "
-                "endpoint={0}, project={1}, uuid={2}"
+        if self.rp is None:
+            logger.debug(
+                'ReportPortal - Init service: '
+                'endpoint={0}, project={1}, uuid={2}'
                 .format(endpoint, project, uuid))
-            RobotService.rp = ReportPortalService(
+            self.rp = ReportPortalService(
                 endpoint=endpoint,
                 project=project,
                 token=uuid,
                 log_batch_size=log_batch_size,
                 max_pool_size=pool_size)
         else:
-            raise Exception("RobotFrameworkService is already initialized")
+            raise RobotServiceException(
+                'RobotFrameworkService is already initialized.')
 
-    @staticmethod
-    def terminate_service():
+    def terminate_service(self):
         """Terminate common reportportal client."""
-        if RobotService.rp is not None:
-            RobotService.rp.terminate()
+        if self.rp is not None:
+            self.rp.terminate()
 
-    @staticmethod
-    def start_launch(launch_name, attributes=None, description=None,
-                     mode=None, ts=None, skip_analytics=False):
+    def start_launch(self, launch, mode=None, ts=None, skip_analytics=False):
         """Call start_launch method of the common client.
 
-        :param launch_name:    Launch name
-        :param attributes:     Launch attributes
-        :param description:    Launch description
+        :param launch:         Instance of the Launch class
         :param mode:           Launch mode
         :param ts:             Start time
         :param skip_analytics: Skip reporting of agent name and version to GA?
         :return:               launch UUID
         """
         sl_pt = {
-            "attributes": RobotService._get_launch_attributes(attributes),
-            "name": launch_name,
-            "start_time": ts or timestamp(),
-            "description": description,
-            "mode": mode
+            'attributes': self._get_launch_attributes(launch.attributes),
+            'description': launch.doc,
+            'name': launch.name,
+            'mode': mode,
+            'start_time': ts or to_epoch(launch.start_time) or timestamp()
         }
-        logging.debug("ReportPortal - Start launch: "
-                      "request_body={0}".format(sl_pt))
+        logger.debug(
+            'ReportPortal - Start launch: request_body={0}'.format(sl_pt))
         if not skip_analytics:
-            send_event(RobotService.agent_name, RobotService.agent_version)
-        return RobotService.rp.start_launch(**sl_pt)
+            send_event(self.agent_name, self.agent_version)
+        return self.rp.start_launch(**sl_pt)
 
-    @staticmethod
-    def finish_launch(launch=None, ts=None):
+    def finish_launch(self, launch, ts=None):
         """Finish started launch.
 
         :param launch: Launch name
         :param ts:     End time
         """
         fl_rq = {
-            "end_time": ts or timestamp(),
-            "status": RobotService.status_mapping[launch.status]
+            'end_time': ts or to_epoch(launch.end_time) or timestamp(),
+            'status': STATUS_MAPPING[launch.status]
         }
-        logging.debug("ReportPortal - Finish launch: "
-                      "request_body={0}".format(fl_rq))
-        RobotService.rp.finish_launch(**fl_rq)
+        logger.debug(
+            'ReportPortal - Finish launch: request_body={0}'.format(fl_rq))
+        self.rp.finish_launch(**fl_rq)
 
-    @staticmethod
-    def start_suite(name=None, suite=None, parent_item_id=None,
-                    attributes=None, ts=None):
+    def start_suite(self, suite, ts=None):
         """Call start_test method of the common client.
 
-        :param name:           Test suite name
-        :param suite:          model.Suite object
-        :param parent_item_id: Parent item UUID
-        :param attributes:     attributes of the test suite
-        :param ts:             Start time
-        :return:               Suite UUID
+        :param suite: model.Suite object
+        :param ts:    Start time
+        :return:      Suite UUID
         """
         start_rq = {
-            "name": name,
-            "attributes": attributes,
-            "description": suite.doc,
-            "start_time": ts or timestamp(),
-            "item_type": "SUITE",
-            "parent_item_id": parent_item_id
+            'attributes': None,
+            'description': suite.doc,
+            'item_type': suite.type,
+            'name': suite.name,
+            'parent_item_id': suite.rp_parent_item_id,
+            'start_time': ts or to_epoch(suite.start_time) or timestamp()
         }
-        logging.debug(
-            "ReportPortal - Start suite: "
-            "request_body={0}".format(start_rq))
-        return RobotService.rp.start_test_item(**start_rq)
+        logger.debug(
+            'ReportPortal - Start suite: request_body={0}'.format(start_rq))
+        return self.rp.start_test_item(**start_rq)
 
-    @staticmethod
-    def finish_suite(item_id, issue=None, suite=None, ts=None):
+    def finish_suite(self, suite, issue=None, ts=None):
         """Finish started suite.
 
-        :param item_id: UUID of the started suite item
-        :param issue:   Corresponding issue if it exists
-        :param suite:   model.Suite object
-        :param ts:      End time
+        :param suite: Instance of the started suite item
+        :param issue: Corresponding issue if it exists
+        :param ts:    End time
         """
         fta_rq = {
-            "end_time": ts or timestamp(),
-            "status": RobotService.status_mapping[suite.status],
-            "issue": issue,
-            "item_id": item_id
+            'end_time': ts or to_epoch(suite.end_time) or timestamp(),
+            'issue': issue,
+            'item_id': suite.rp_item_id,
+            'status': STATUS_MAPPING[suite.status]
         }
-        logging.debug(
-            "ReportPortal - Finish suite:"
-            " request_body={0}".format(fta_rq))
-        RobotService.rp.finish_test_item(**fta_rq)
+        logger.debug(
+            'ReportPortal - Finish suite: request_body={0}'.format(fta_rq))
+        self.rp.finish_test_item(**fta_rq)
 
-    @staticmethod
-    def start_test(test=None, parent_item_id=None, attributes=None, ts=None):
+    def start_test(self, test, ts=None):
         """Call start_test method of the common client.
 
-        :param test:           model.Test object
-        :param parent_item_id: Parent item UUID
-        :param attributes:     attributes of the test case
-        :param ts:             Start time
+        :param test: model.Test object
+        :param ts:   Start time
         """
         # Item type should be sent as "STEP" until we upgrade to RPv6.
         # Details at:
         # https://github.com/reportportal/agent-Python-RobotFramework/issues/56
         start_rq = {
-            "name": test.name,
-            "attributes": attributes,
-            "description": test.doc,
-            "start_time": ts or timestamp(),
-            "item_type": "STEP",
-            "parent_item_id": parent_item_id
+            'attributes': test.attributes,
+            'description': test.doc,
+            'item_type': 'STEP',
+            'name': test.name,
+            'parent_item_id': test.rp_parent_item_id,
+            'start_time': ts or to_epoch(test.start_time) or timestamp()
         }
-        logging.debug(
-            "ReportPortal - Start test: "
-            "request_body={0}".format(start_rq))
-        return RobotService.rp.start_test_item(**start_rq)
+        logger.debug(
+            'ReportPortal - Start test: request_body={0}'.format(start_rq))
+        return self.rp.start_test_item(**start_rq)
 
-    @staticmethod
-    def finish_test(item_id, issue=None, test=None, ts=None):
+    def finish_test(self, test, issue=None, ts=None):
         """Finish started test case.
 
-        :param item_id: UUID of the started test item
-        :param issue:   Corresponding issue if it exists
-        :param test:    model.Test object
-        :param ts:      End time
+        :param test:  Instance of started test item
+        :param issue: Corresponding issue if it exists
+        :param ts:    End time
         """
         fta_rq = {
-            "end_time": ts or timestamp(),
-            "status": RobotService.status_mapping[test.status],
-            "issue": issue,
-            "item_id": item_id
+            'end_time': ts or to_epoch(test.end_time) or timestamp(),
+            'issue': issue,
+            'item_id': test.rp_item_id,
+            'status': STATUS_MAPPING[test.status]
         }
-        logging.debug(
-            "ReportPortal - Finish test:"
-            " request_body={0}".format(fta_rq))
-        RobotService.rp.finish_test_item(**fta_rq)
+        logger.debug(
+            'ReportPortal - Finish test: request_body={0}'.format(fta_rq))
+        self.rp.finish_test_item(**fta_rq)
 
-    @staticmethod
-    def start_keyword(keyword=None, parent_item_id=None, has_stats=True,
-                      ts=None):
+    def start_keyword(self, keyword, ts=None):
         """Call start_test method of the common client.
 
-        :param keyword:        model.Keyword object
-        :param parent_item_id: Parent item UUID
-        :param has_stats:      Flag that indicated whether keyword needs to
-                               be nested step or not. (False - nested)
-        :param ts:             Start time
+        :param keyword: model.Keyword object
+        :param ts:      Start time
         """
         start_rq = {
-            "name": keyword.get_name(),
-            "description": keyword.doc,
-            "start_time": ts or timestamp(),
-            "item_type": keyword.get_type(),
-            "parent_item_id": parent_item_id,
-            "has_stats": has_stats
+            'description': keyword.doc,
+            'has_stats': False if keyword.parent_type == 'KEYWORD' else True,
+            'item_type': keyword.get_type(),
+            'name': keyword.get_name(),
+            'parent_item_id': keyword.rp_parent_item_id,
+            'start_time': ts or to_epoch(keyword.start_time) or timestamp()
         }
-        logging.debug(
-            "ReportPortal - Start keyword: "
-            "request_body={0}".format(start_rq))
-        return RobotService.rp.start_test_item(**start_rq)
+        logger.debug(
+            'ReportPortal - Start keyword: request_body={0}'.format(start_rq))
+        return self.rp.start_test_item(**start_rq)
 
-    @staticmethod
-    def finish_keyword(item_id, issue=None, keyword=None, ts=None):
-        """Finish started test case.
+    def finish_keyword(self, keyword, issue=None, ts=None):
+        """Finish started keyword item.
 
-        :param item_id: UUID of the started test item
+        :param keyword: Instance of started keyword item
         :param issue:   Corresponding issue if it exists
-        :param keyword: model.Keyword object
         :param ts:      End time
         """
         fta_rq = {
-            "end_time": ts or timestamp(),
-            "status": RobotService.status_mapping[keyword.status],
-            "issue": issue,
-            "item_id": item_id
+            'end_time': ts or to_epoch(keyword.end_time) or timestamp(),
+            'issue': issue,
+            'item_id': keyword.rp_item_id,
+            'status': STATUS_MAPPING[keyword.status]
         }
-        logging.debug(
-            "ReportPortal - Finish keyword:"
-            " request_body={0}".format(fta_rq))
-        RobotService.rp.finish_test_item(**fta_rq)
+        logger.debug(
+            'ReportPortal - Finish keyword: request_body={0}'.format(fta_rq))
+        self.rp.finish_test_item(**fta_rq)
 
-    @staticmethod
-    def log(message, ts=None):
+    def log(self, message, ts=None):
         """Send log message to Report Portal.
 
         :param message: model.LogMessage object
         :param ts:      Timestamp
         """
         sl_rq = {
-            "time": ts or timestamp(),
-            "message": message.message,
-            "level": RobotService.log_level_mapping[message.level],
-            "attachment": message.attachment,
-            "item_id": message.item_id
+            'attachment': message.attachment,
+            'item_id': message.item_id,
+            'level': LOG_LEVEL_MAPPING[message.level],
+            'message': message.message,
+            'time': ts or timestamp()
         }
-        RobotService.rp.log(**sl_rq)
+        self.rp.log(**sl_rq)
---
+++ robotframework_reportportal/static.py
@@ -0,0 +1,34 @@
+"""This module includes static variables of the agent.
+
+Copyright (c) 2021 http://reportportal.io .
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
+LOG_LEVEL_MAPPING = {
+    'INFO': 'INFO',
+    'FAIL': 'ERROR',
+    'TRACE': 'TRACE',
+    'DEBUG': 'DEBUG',
+    'HTML': 'INFO',
+    'WARN': 'WARN',
+    'ERROR': 'ERROR'
+}
+MAIN_SUITE_ID = 's1'
+PABOT_WIHOUT_LAUNCH_ID_MSG = (
+    'Pabot library is used but RP_LAUNCH_UUID was not provided. Please, '
+    'initialize listener with the RP_LAUNCH_UUID argument.')
+STATUS_MAPPING = {
+    'PASS': 'PASSED',
+    'FAIL': 'FAILED',
+    'NOT RUN': 'SKIPPED',
+    'SKIP': 'SKIPPED'
+}
