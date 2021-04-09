f0b4f4935485c1f756327588c25d7d08ff75dd99
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-04-09 15:39:21 +0300

    A method argument name update

--- tests/test_listener.py
+++ tests/test_listener.py
@@ -7,26 +7,26 @@ REPORT_PORTAL_SERVICE = \
 class TestListener:
 
     @mock.patch(REPORT_PORTAL_SERVICE)
-    def test_critical_test_failure(self, mock_init, mock_listener,
+    def test_critical_test_failure(self, mock_client_init, mock_listener,
                                    test_attributes):
         mock_listener.start_test('Test', test_attributes)
         test_attributes['status'] = 'FAIL'
         mock_listener.end_test('Test', test_attributes)
 
-        mock_client = mock_init.return_value
+        mock_client = mock_client_init.return_value
         assert mock_client.finish_test_item.call_count == 1
         args, kwargs = mock_client.finish_test_item.call_args
         assert kwargs['status'] == 'FAILED'
 
     @mock.patch(REPORT_PORTAL_SERVICE)
-    def test_non_critical_test_skip(self, mock_init, mock_listener,
+    def test_non_critical_test_skip(self, mock_client_init, mock_listener,
                                     test_attributes):
         test_attributes['critical'] = 'no'
         mock_listener.start_test('Test', test_attributes)
         test_attributes['status'] = 'FAIL'
         mock_listener.end_test('Test', test_attributes)
 
-        mock_client = mock_init.return_value
+        mock_client = mock_client_init.return_value
         assert mock_client.finish_test_item.call_count == 1
         args, kwargs = mock_client.finish_test_item.call_args
         assert kwargs['status'] == 'SKIPPED'
