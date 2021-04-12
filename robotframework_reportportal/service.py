cef402515fb7f77210978b3f3bf541fc3b55112e
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-04-12 19:42:21 +0300

    Keywords should not have stats

--- robotframework_reportportal/service.py
+++ robotframework_reportportal/service.py
@@ -218,7 +218,7 @@ class RobotService(object):
         """
         start_rq = {
             'description': keyword.doc,
-            'has_stats': False if keyword.parent_type == 'KEYWORD' else True,
+            'has_stats': False,
             'item_type': keyword.get_type(),
             'name': keyword.get_name(),
             'parent_item_id': keyword.rp_parent_item_id,
