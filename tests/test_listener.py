f7dfe748175efce4161eabe4bcae51cebd2b8fcc
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-04-13 12:19:16 +0300

    skipped_issue tests

--- tests/test_listener.py
+++ tests/test_listener.py
@@ -1,4 +1,7 @@
+import pytest
 from six.moves import mock
+
+from robotframework_reportportal.listener import listener
 
 REPORT_PORTAL_SERVICE = \
     'robotframework_reportportal.service.ReportPortalService'
@@ -30,3 +33,17 @@ class TestListener:
         assert mock_client.finish_test_item.call_count == 1
         args, kwargs = mock_client.finish_test_item.call_args
         assert kwargs['status'] == 'SKIPPED'
+
+    @mock.patch(REPORT_PORTAL_SERVICE)
+    @pytest.mark.parametrize('skipped_issue_value', [True, False])
+    def test_skipped_issue_variable_bypass(self, mock_client_init,
+                                           mock_variables,
+                                           skipped_issue_value):
+        mock_variables.skipped_issue = skipped_issue_value
+        mock_listener = listener()
+        mock_listener._variables = mock_variables
+        mock_listener.service
+
+        assert mock_client_init.call_count == 1
+        args, kwargs = mock_client_init.call_args
+        assert kwargs['is_skipped_an_issue'] == skipped_issue_value
