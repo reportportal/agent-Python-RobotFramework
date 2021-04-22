0e605cba03629b4c3bfa169e15b18d63358e6d75
Ivan Ivanou <8349078+iivanou@users.noreply.github.com>
2021-04-22 13:27:10 +0300

    Implement functionality for sending test case id

--- tests/test_listener.py
+++ tests/test_listener.py
@@ -7,6 +7,16 @@ REPORT_PORTAL_SERVICE = \
 class TestListener:
 
     @mock.patch(REPORT_PORTAL_SERVICE)
+    def test_code_ref(self, mock_client_init, mock_listener,
+                      test_attributes):
+        mock_listener.start_test('Test', test_attributes)
+        mock_client = mock_client_init.return_value
+        assert mock_client.start_test_item.call_count == 1
+        args, kwargs = mock_client.start_test_item.call_args
+        assert (kwargs['code_ref'] ==
+                '{0}:{1}'.format(test_attributes['source'], 'Test'))
+
+    @mock.patch(REPORT_PORTAL_SERVICE)
     def test_critical_test_failure(self, mock_client_init, mock_listener,
                                    test_attributes):
         mock_listener.start_test('Test', test_attributes)
@@ -30,3 +40,14 @@ class TestListener:
         assert mock_client.finish_test_item.call_count == 1
         args, kwargs = mock_client.finish_test_item.call_args
         assert kwargs['status'] == 'SKIPPED'
+
+    @mock.patch(REPORT_PORTAL_SERVICE)
+    def test_test_case_id(self, mock_client_init, mock_listener,
+                          test_attributes):
+        test_attributes['tags'] = ['simple', 'test_case_id:12345']
+        mock_listener.start_test('Test', test_attributes)
+        mock_client = mock_client_init.return_value
+        assert mock_client.start_test_item.call_count == 1
+        args, kwargs = mock_client.start_test_item.call_args
+        assert kwargs['test_case_id'] == '12345'
+        assert kwargs['attributes'] == [{'value': 'simple'}]
