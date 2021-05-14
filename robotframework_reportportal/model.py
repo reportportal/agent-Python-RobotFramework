6be6fe885d8a0e865e54797098a94e2ec0b513e4
Ivan Ivanou <8349078+iivanou@users.noreply.github.com>
2021-05-14 16:38:02 +0300

    Update model.py

--- robotframework_reportportal/model.py
+++ robotframework_reportportal/model.py
@@ -11,6 +11,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+from six import text_type
+
 from reportportal_client.service import _convert_string
 
 
@@ -178,7 +180,7 @@ class Keyword(object):
         return self
 
 
-class LogMessage(object):
+class LogMessage(text_type):
     """Class represents Robot Framework messages."""
 
     def __init__(self, message):
