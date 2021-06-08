e6940204e5659208e3a693b64026f42ff7344950
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2021-06-08 16:36:23 +0300

    PR fixes

--- tests/unit/test_logger.py
+++ tests/unit/test_logger.py
@@ -13,8 +13,6 @@ See the License for the specific language governing permissions and
 limitations under the License
 """
 
-from itertools import product, chain
-
 import pytest
 from delayed_assert import assert_expectations, expect
 from six.moves import mock
@@ -24,45 +22,58 @@ from robotframework_reportportal import logger
 ATTACHMENT = {'name': 'test_screenshot.png', 'data': b'x0x0',
               'mime': 'image/png'}
 
-BASIC_PARAMS = [
-    {'msg': None, 'html': False, 'attachment': None, 'launch_log': False},
-    {'msg': 'Test', 'html': False, 'attachment': None, 'launch_log': False},
-    {'msg': None, 'html': True, 'attachment': None, 'launch_log': False},
-    {'msg': None, 'html': False, 'attachment': ATTACHMENT,
-     'launch_log': False},
-    {'msg': None, 'html': False, 'attachment': None, 'launch_log': True}]
-
-INFO_PARAMS = [{k: v for k, v in chain(e.items(), [('also_console', False)])}
-               for e in BASIC_PARAMS + [BASIC_PARAMS[0]]]
-INFO_PARAMS[-1]['also_console'] = True
-INFO_PARAMS[-1]['msg'] = 'Test console message'
-
-TEST_DATA_METHODS = chain(
-    product(['trace', 'debug', 'warn', 'error'], BASIC_PARAMS),
-    product(['info'], INFO_PARAMS))
+TEST_DATA_METHODS = [
+    ('trace', ['Test', False, None, False]),
+    ('trace', [None, True, None, False]),
+    ('trace', [None, False, ATTACHMENT, False]),
+    ('trace', [None, False, None, True]),
+    ('debug', ['Test', False, None, False]),
+    ('debug', [None, True, None, False]),
+    ('debug', [None, False, ATTACHMENT, False]),
+    ('debug', [None, False, None, True]),
+    ('warn', ['Test', False, None, False]),
+    ('warn', [None, True, None, False]),
+    ('warn', [None, False, ATTACHMENT, False]),
+    ('warn', [None, False, None, True]),
+    ('error', ['Test', False, None, False]),
+    ('error', [None, True, None, False]),
+    ('error', [None, False, ATTACHMENT, False]),
+    ('error', [None, False, None, True]),
+    ('info', ['Test', False, False, None, False]),
+    ('info', [None, True, False, None, False]),
+    ('info', [None, False, True, None, False]),
+    ('info', [None, False, False, ATTACHMENT, False]),
+    ('info', [None, False, False, None, True]),
+]
 
 
 @pytest.mark.parametrize('method, params', TEST_DATA_METHODS)
 @mock.patch('robotframework_reportportal.logger.logger')
 def test_logger_params_bypass(mock_logger, method, params):
-    getattr(logger, method)(**params)
+    getattr(logger, method)(*params)
     assert mock_logger.write.call_count == 1
 
-    message = mock_logger.write.call_args[0][0]
-    expect(message.level == method.upper())
-    expect(message.message == params['msg'])
-    expect(message.attachment == params['attachment'])
-    expect(message.launch_log == params['launch_log'])
+    if method == 'info':
+        attachment = params[3]
+        launch_log = params[4]
+    else:
+        attachment = params[2]
+        launch_log = params[3]
 
-    expect(mock_logger.write.call_args[0][1] == method.upper())
-    expect(mock_logger.write.call_args[0][2] == params['html'])
-
-    if 'also_console' in params and params['also_console']:
+    if method == 'info' and params[2]:
         expect(mock_logger.console.call_count == 1)
-        expect(mock_logger.console.call_args[0][0] == params['msg'])
+        expect(mock_logger.console.call_args[0][0] == params[0])
         expect(mock_logger.console.call_args[0][1] is True)
         expect(mock_logger.console.call_args[0][2] == 'stdout')
     else:
         expect(mock_logger.console.call_count == 0)
+
+    message = mock_logger.write.call_args[0][0]
+    expect(message.level == method.upper())
+    expect(message.message == params[0])
+    expect(message.attachment == attachment)
+    expect(message.launch_log == launch_log)
+    expect(mock_logger.write.call_args[0][1] == method.upper())
+    expect(mock_logger.write.call_args[0][2] == params[1])
 
     assert_expectations()
