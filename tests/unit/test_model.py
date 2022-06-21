68e047aa6673a2c6c4a82101d7a69d4e3cc9d846
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-06-21 14:56:06 +0300

    Copyright fixes

--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -1,10 +1,10 @@
 """This module includes unit tests for the model.py module.
 
-Copyright (c) 2021 http://reportportal.io .
+Copyright (c) 2021 https://reportportal.io .
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-http://www.apache.org/licenses/LICENSE-2.0
+https://www.apache.org/licenses/LICENSE-2.0
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
