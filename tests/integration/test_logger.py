8d428c06ee7972e04509afda744a2a53b9e9022c
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2025-12-29 16:42:14 +0300

    Fix tests

--- tests/integration/test_logger.py
+++ tests/integration/test_logger.py
@@ -12,7 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import platform
 from unittest import mock
 
 from tests import REPORT_PORTAL_SERVICE
